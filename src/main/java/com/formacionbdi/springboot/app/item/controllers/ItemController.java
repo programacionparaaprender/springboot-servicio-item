75d8645e4996ca65bedad40bd182b08c43c99510
programacionparaaprender <yancel209@gmail.com>
2024-02-18 17:19:47 -0500

    agregando cambios

--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -13,12 +13,15 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +32,9 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RefreshScope
 @RestController
+//@RequestMapping("/item") //localhost:8080/item
+@CrossOrigin
+@Controller
 public class ItemController {
 	
 	private static Logger log = LoggerFactory.getLogger(ItemController.class);
@@ -37,7 +43,8 @@ public class ItemController {
 	private Environment env;
 	
 	@Autowired
-	@Qualifier("serviceFeign")
+	//@Qualifier("serviceFeign")
+	@Qualifier("serviceRestTemplate")
 	private ItemService itemService;
 	
 	@Value("${configuracion.texto}")
